55352cbd27f0f8ac0d635134762ef33325da52dd
Jorge Flores Carlos <florescarlos167@gmail.com>
2023-02-02 13:37:31 -0600

    clase 5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
     println!("Por favor introduce tu nombre: ");
-    
+
     let mut nombre: String = String::new();
-     
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+
+    // obtener la edad de la consola
+    println!("Por favor introduce tu edad: ");
+    let mut edad : String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+
+    // Convertir esa edad a numero
+    let edad_int : u8 = edad.trim().parse().unwrap();
+
+    println!("Hola, bienvenido o bienvenida {} de {} años", nombre, edad_int);
 }
